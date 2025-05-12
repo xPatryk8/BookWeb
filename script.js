ebb5fca366d9d65c7b5976496a9bcc87339bfca1
Patryk <patrykzuk.pz@gmail.com>
2025-05-12 19:37:55 +0200

    Fetch function with user data

--- script.js
+++ script.js
@@ -11,3 +11,38 @@ function showCheckboxes() {
 		show = true;
 	}
 }
+
+function getData() {
+	let checkboxes = document.getElementsByName("genre");
+	let result = "";
+
+	for (var i = 0; i < checkboxes.length; i++) {
+		if (checkboxes[i].checked) {
+			result += checkboxes[i].value + "+";
+		}
+	}
+
+	return result;
+}
+
+async function getBooks() {
+	let url = "https://openlibrary.org/search.json?subject=" + getData();
+
+	try {
+		const response = await fetch(url, {
+			method: "GET",
+		});
+		if (!response.ok) {
+			throw new Error(`Response status: ${response.status}`);
+		}
+
+		const json = await response.json();
+		return json;
+	} catch (error) {
+		console.error(error.message);
+	}
+}
+
+function showBooks() {
+	console.log(getBooks());
+}
