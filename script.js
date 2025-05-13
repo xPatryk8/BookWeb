d291dfe8048ee76d0a1fa1f21054f2c8e4458adb
Patryk <patrykzuk.pz@gmail.com>
2025-05-13 18:57:17 +0200

    fetching data and creating HTML element

--- script.js
+++ script.js
@@ -22,11 +22,21 @@ function getData() {
 		}
 	}
 
+	console.log(result);
 	return result;
 }
 
 async function getBooks() {
+	/* 	const subjectData = getData();
+	if (subjectData) {
+		// Check if subjectData is not empty
+		console.log("ok");
+	} else {
+		console.log("Please select at least one genre.");
+	} */
+
 	let url = "https://openlibrary.org/search.json?subject=" + getData();
+	console.log("URL:" + url);
 
 	try {
 		const response = await fetch(url, {
@@ -37,6 +47,7 @@ async function getBooks() {
 		}
 
 		const json = await response.json();
+		console.log(json);
 		return json;
 	} catch (error) {
 		console.error(error.message);
@@ -44,5 +55,26 @@ async function getBooks() {
 }
 
 function showBooks() {
-	console.log(getBooks());
+	window.onload = function () {
+		let results = getElementById("results");
+	};
+	let book = document.createElement("section");
+	book.className = "books";
+	book.innerHTML = `<section class="book">
+	<div class="cover">
+		<img class="book_cover" src="design.png" alt="" />
+	</div>
+	<div class="book_info">
+		<h2 class="book_title">Book Title</h2>
+		<p class="author_name">Author Name</p>
+		<p class="descripttion">Lorem ipsorem ipsum dolororem lor sit amet</p>
+		<p class="more_info">
+			<a href="" target="_blank">
+				More Info
+			</a>
+		</p>
+	</div>
+</section>`;
+
+	results.appendChild(book);
 }
