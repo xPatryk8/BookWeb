62800ea23cad8fe8fa6aafd30c7c17618d81f1c5
Patryk <patrykzuk.pz@gmail.com>
2025-05-25 13:42:30 +0200

    Migrating to React

--- GenBook/src/App.tsx
+++ GenBook/src/App.tsx
@@ -1,35 +1,151 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+function Genres() {
+	const genres = [
+		{ key: 0, name: "Adventure" },
+		{ key: 1, name: "Detective" },
+		{ key: 2, name: "Dystopian" },
+		{ key: 3, name: "Fantasy" },
+		{ key: 4, name: "Historical Fiction" },
+		{ key: 5, name: "History" },
+		{ key: 6, name: "Horror" },
+		{ key: 7, name: "Mystery" },
+		{ key: 8, name: "Poetry" },
+		{ key: 9, name: "Psychology" },
+		{ key: 10, name: "Romance" },
+		{ key: 11, name: "Sci-Fi" },
+		{ key: 12, name: "Young Adult" },
+	];
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const [searchItem, setSearchItem] = useState("");
+	const [filteredGenres, setFilteredGenres] = useState(genres);
+
+	// TODO: fix searching
+	const handleInput = (searchValue: string) => {
+		setSearchItem(searchValue);
+		if (searchItem !== " ") {
+			const filtered = genres.filter((item) => {
+				return Object.values(item).join("").toLowerCase().includes(searchItem.toLowerCase());
+			});
+			setFilteredGenres(filtered);
+		} else {
+			setFilteredGenres(genres);
+		}
+	};
+
+	return (
+		<>
+			<input type="text" value={searchItem} onChange={(e) => handleInput(e.target.value)} name="searchGenre" id="SearchGenre" placeholder="Search genre..." />
+			<div id="genreList">
+				{filteredGenres.map((gen) => (
+					<label key={gen.key}>
+						<input type="checkbox" name={gen.name} id={gen.name} />
+						{gen.name}
+					</label>
+				))}
+			</div>
+		</>
+	);
 }
 
-export default App
+function Languages() {
+	const languages = [
+		{ key: 0, name: "English" },
+		{ key: 1, name: "French" },
+		{ key: 2, name: "German" },
+		{ key: 3, name: "Italian" },
+		{ key: 4, name: "Polish" },
+		{ key: 5, name: "Russian" },
+		{ key: 6, name: "Spanish" },
+	];
+
+	return (
+		<>
+			<h4>Languages</h4>
+			<div id="languageList">
+				{languages.map((lang) => (
+					<label key={lang.key}>
+						<input type="radio" name="language" id={lang.name} /> {lang.name}
+					</label>
+				))}
+			</div>
+		</>
+	);
+}
+
+function Results() {
+	return (
+		<section id="results" className="results">
+			LOL
+		</section>
+	);
+}
+
+function App() {
+	return (
+		<>
+			<header>
+				<h1>Random Book Generator</h1>
+			</header>
+			<main>
+				<nav>
+					<Genres></Genres>
+					<Languages></Languages>
+				</nav>
+				<div id="books">
+					<Results></Results>
+				</div>
+			</main>
+
+			{/* <>
+				
+				<main>
+					<form>
+						<div className="multipleSelection">
+							<div className="selectBox">
+								<select>
+									<option>Select options</option>
+									<option value="test">yitufvytui</option>
+								</select>
+								<div className="overSelect" />
+							</div>
+							<div id="checkBoxes">
+								<label htmlFor="action">
+									<input type="checkbox" name="genre" defaultValue="action" id="action" />
+									Action
+								</label>
+								<br />
+								<label htmlFor="horror">
+									<input type="checkbox" name="genre" defaultValue="horror" id="horror" />
+									Horror
+								</label>
+								<br />
+								<label htmlFor="romance">
+									<input type="checkbox" name="genre" defaultValue="romance" id="romance" />
+									Romance
+								</label>
+								<br />
+								<label htmlFor="biography">
+									<input type="checkbox" name="genre" defaultValue="biography" id="biography" />
+									Biography
+								</label>
+								<br />
+								<label htmlFor="adventure">
+									<input type="checkbox" name="genre" defaultValue="adventure" id="adventure" />
+									Adventure
+								</label>
+								<br />
+							</div>
+						</div>
+						<input type="text" name="author" id="author" placeholder="Author Name" />
+						<input type="button" id="generate" defaultValue="Generate Book" />
+					</form>
+					<section id="results" className="results" />
+				</main>
+			</> */}
+		</>
+	);
+}
+
+export default App;
